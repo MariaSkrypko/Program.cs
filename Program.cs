cab32325522831f7cbe096b8b22171c3209bf4d0
Maria <mfusxr@gmail.com>
2024-11-07 01:12:28 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+class Task1
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter_1 some phrase: ");
+        string input = Console.ReadLine();
+
+        StringBuilder sb = new StringBuilder(input);
+        for(int i = 0; i < sb.Length; i++)
+        {
+            sb[i] = Char.ToUpper(sb[i]);
+        }
+        Console.WriteLine("UpperCase: " + sb.ToString());
+        Console.WriteLine();
+
+
+
+        Console.WriteLine("Enter_2 some phrase: ");
+        string input2 = Console.ReadLine();
+        string result = input2.ToLower();
+        Console.WriteLine("LowerCase: " + result);
+    }
+}
